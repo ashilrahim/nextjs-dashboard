882ed41b1c6deb61fb96cbed145d3b152063fd60
ashil rahim <ashilrahim2@gmail.com>
2025-12-14 12:07:41 +0530

    made some changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,15 @@ import AppBarChart from "@/components/AppBarChart";
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2  gap-4 ">
-      <div className="bg-primary-foreground rounded-lg ">
+    <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4   gap-4 ">
+      
+      <div className="bg-primary-foreground lg:col-span-2  rounded-lg ">
         <AppBarChart />
       </div>
-      <div className="bg-primary-foreground p-10 rounded-lg ">Test</div>
-      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground  rounded-lg ">Test</div>
+      <div className="bg-primary-foreground  rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4  rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 lg:col-span-2 rounded-lg">Test</div>
       <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
 
 
